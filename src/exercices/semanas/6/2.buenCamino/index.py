2fd38162c0075a2dd36d749e0121c9f6814f2e4f
Jorge Manrique <georgemanc.28@gmail.com>
2024-04-18 16:19:14 -0500

    Se agrega ejercicio buen camino con Python

---
+++ src/exercices/semanas/6/2.buenCamino/index.py
@@ -0,0 +1,11 @@
+"""
+Buen camino:Escribir un programa que solicite al usuario su nombre y edad, e imprima
+Enunciado
+Buen camino: Escribir un programa que solicite al usuario su nombre y edad, e imprima : “Vas por buen camino[usuario]”
+Dificultad
+"""
+
+nombre = input('Ingrese su nombre ')
+edad = int(input('Ingrese su edad '))
+
+print(f'Vas por buen camino {nombre}, su edad es {edad} años.')
