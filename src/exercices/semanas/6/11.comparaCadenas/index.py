b282a0d1836723246f80a24e7d633633ea39e5d7
Jorge Manrique <georgemanc.28@gmail.com>
2024-04-18 17:22:44 -0500

    Compara cadenas

---
+++ src/exercices/semanas/6/11.comparaCadenas/index.py
@@ -0,0 +1,17 @@
+""" 
+Comparacion cadenas
+Enunciado
+Comparación cadenas: Solicita al usuario dos cadenas compáralas y verifica si son o no iguales
+Dificultad
+Media
+
+Instrucciones
+"""
+
+#Comparacion de cadenas
+
+cadena1 = input("Ingrese la primera cadena: ")
+cadena2 = input("Ingrese la segunda cadena: ")
+
+son_iguales = cadena1 == cadena2
+print(f"¿Las cadenas son iguales? {son_iguales}")
