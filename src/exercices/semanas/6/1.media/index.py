1ae240b53d80d4118f4539682faa1fab1439bdc9
Jorge Manrique <georgemanc.28@gmail.com>
2024-04-18 16:14:12 -0500

    Se inicia a trabajar con Python

---
+++ src/exercices/semanas/6/1.media/index.py
@@ -0,0 +1,7 @@
+numero1 = int(input('Ingrese el número 1 '))
+numero2 = int(input('Ingrese el número 2 '))
+numero3 = int(input('Ingrese el número 3 '))
+
+media = (numero1 + numero2 + numero3)/3
+
+print(f'La media es: {media}')
